f35aaa879a2384a75c7cf92c4337e7a78105e008
menaka <shakya.menaka@gmail.com>
2025-06-09 14:04:38 +0200

    ajoute bouton a filtre forEach

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -15,28 +15,52 @@ fetch("http://localhost:5678/api/works")
 
 	});
 
+// Array to hold the filter buttons
+const filterButtons = ['Tous', 'Objets', 'Appartements', 'Hôtels & restaurants'];
+const categories = ['all', 'objects', 'apartments', 'hotels'];
+
 // Sélectionner la section de filtre
 const filterSection = document.getElementById('filter');
 
+filterButtons.forEach(buttonText => {
+	let button = document.createElement('button');
+	button.classList.add('filter-button');
+	button.innerText = buttonText;
+	button.setAttribute = ('filter-data');
+	filterSection.appendChild(button);
+	});
+
+// Ajouter un écouteur d'événement pour chaque bouton de filtre
+
+
+    
+
+
+
+
+
+
+
+
 // Créer les nouveau boutons
-const allButton = document.createElement('button');
-allButton.classList.add('filter-button');
-allButton.innerText = 'Tous'
+//const allButton = document.createElement('button');
+//allButton.classList.add('filter-button');
+//allButton.innerText = 'Tous'
 
-const objectsButton = document.createElement('button');
-objectsButton.classList.add('filter-button');
-objectsButton.innerText = 'Objets';
+//const objectsButton = document.createElement('button');
+//objectsButton.classList.add('filter-button');
+//objectsButton.innerText = 'Objets';
 
-const apartmentsButton = document.createElement('button');
-apartmentsButton.classList.add('filter-button');
-apartmentsButton.innerText = 'Apartments';
+//const apartmentsButton = document.createElement('button');
+//apartmentsButton.classList.add('filter-button');
+//apartmentsButton.innerText = 'Apartments';
 
-const hotelsButton = document.createElement('button');
-hotelsButton.classList.add('filter-button');
-hotelsButton.innerText = 'Hotels & restaurants';
+//const hotelsButton = document.createElement('button');
+//hotelsButton.classList.add('filter-button');
+//hotelsButton.innerText = 'Hotels & restaurants';
 
 // Ajouter les nouveau boutons dans la section de filtre
-filterSection.appendChild(allButton);
-filterSection.appendChild(objectsButton);
-filterSection.appendChild(apartmentsButton);
-filterSection.appendChild(hotelsButton);
+//filterSection.appendChild(allButton);
+//filterSection.appendChild(objectsButton);
+//filterSection.appendChild(apartmentsButton);
+//filterSection.appendChild(hotelsButton);

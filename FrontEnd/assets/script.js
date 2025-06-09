891e82369ea9718b1a3560108d5d0bbfedd90110
menaka <shakya.menaka@gmail.com>
2025-06-09 12:15:02 +0200

    ajout des boutons filtre en java script

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,3 +1,4 @@
+// Fetch works from the API and display them in the gallery
 fetch("http://localhost:5678/api/works")
 	.then(response => response.json())
 	.then(data => {
@@ -13,3 +14,29 @@ fetch("http://localhost:5678/api/works")
 		);
 
 	});
+
+// Sélectionner la section de filtre
+const filterSection = document.getElementById('filter');
+
+// Créer les nouveau boutons
+const allButton = document.createElement('button');
+allButton.classList.add('filter-button');
+allButton.innerText = 'Tous'
+
+const objectsButton = document.createElement('button');
+objectsButton.classList.add('filter-button');
+objectsButton.innerText = 'Objets';
+
+const apartmentsButton = document.createElement('button');
+apartmentsButton.classList.add('filter-button');
+apartmentsButton.innerText = 'Apartments';
+
+const hotelsButton = document.createElement('button');
+hotelsButton.classList.add('filter-button');
+hotelsButton.innerText = 'Hotels & restaurants';
+
+// Ajouter les nouveau boutons dans la section de filtre
+filterSection.appendChild(allButton);
+filterSection.appendChild(objectsButton);
+filterSection.appendChild(apartmentsButton);
+filterSection.appendChild(hotelsButton);
